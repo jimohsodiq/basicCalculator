a68b084c48c2a031d35176a2504d46b44b7edfdd
Jimoh Sodiq <serikidon32@gmail.com>
2022-06-22 14:52:57 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let firstNum = 20;
+let secondNum = 8;
+
+console.log(`Results: ${firstNum + secondNum}`);
+console.log(`Results: ${firstNum - secondNum}`);
+console.log(`Results: ${firstNum / secondNum}`);
+console.log(`Results: ${firstNum * secondNum}`);
+console.log(`Results: ${firstNum % secondNum}`);
